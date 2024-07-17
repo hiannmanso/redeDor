0af567841dc5b8cdcdad0675506291d7f7f3d0a9
hiann <hiannmanso@gmail.com>
2024-07-16 22:34:48 -0300

    feat: cep project

--- client/cypress/e2e/1-getting-started/cepForm.cy.js
+++ client/cypress/e2e/1-getting-started/cepForm.cy.js
@@ -1,25 +1,25 @@
 describe("Consulta de CEP", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:5173"); // URL do seu app rodando com Vite
+    cy.visit("http://localhost:5173"); 
   });
 
   it("deve retornar um endereço válido para um CEP existente", () => {
-    const cep = "21210-140"; // Substitua pelo CEP que você está testando
+    const cep = "21210-140"; 
 
     cy.get("input#cep").type(cep);
     cy.get("button").contains("Enviar").click();
 
-    cy.get(".bg-white").should("exist"); // Verifica se o endereço foi exibido
-    cy.get(".bg-white").contains("Praça Nupeba"); // Verifica se o endereço correto foi exibido
+    cy.get(".bg-white").should("exist");
+    cy.get(".bg-white").contains("Praça Nupeba"); 
   });
 
   it("deve mostrar um erro para um CEP inválido", () => {
-    const cep = "00000-000"; // Substitua por um CEP que você sabe que não existe
+    const cep = "00000-000"; 
 
     cy.get("input#cep").type(cep);
     cy.get("button").contains("Enviar").click();
 
-    cy.get(".bg-red-100").should("exist"); // Verifica se a mensagem de erro foi exibida
-    cy.get(".bg-red-100").contains("CEP não encontrado ou inválido."); // Verifica a mensagem de erro
+    cy.get(".bg-red-100").should("exist"); 
+    cy.get(".bg-red-100").contains("CEP não encontrado ou inválido."); 
   });
 });
